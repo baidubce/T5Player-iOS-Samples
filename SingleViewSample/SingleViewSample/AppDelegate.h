33614863060026c4b3b4127d68a0f970dd8f533f
Damon Hu <damon.hu@gmail.com>
2015-11-30 11:16:44 +0800

    Add empty single view sample

---
+++ SingleViewSample/SingleViewSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SingleViewSample
+//
+//  Created by hudapeng on 11/30/15.
+//  Copyright © 2015 Baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SingleViewSample/SingleViewSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SingleViewSample
+//
+//  Created by hudapeng on 11/30/15.
+//  Copyright © 2015 Baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
