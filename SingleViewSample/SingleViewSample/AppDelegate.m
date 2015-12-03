3ba55dce74b6b2921910612f60c99e4273d16e7c
Damon Hu <damon.hu@gmail.com>
2015-12-03 23:14:56 +0800

    add libbz

--- SingleViewSample/SingleViewSample/AppDelegate.m
+++ SingleViewSample/SingleViewSample/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"Screen size: %@", NSStringFromCGRect([[UIScreen mainScreen] bounds]));
     return YES;
 }
 
